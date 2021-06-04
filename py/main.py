1d36441fde62fb6333d36c086f7a594513743dd1
SergeAlexandre <sa@broadsoftware.com>
2021-06-04 12:22:35 +0200

    Handle role switching

--- py/main.py
+++ py/main.py
@@ -4,30 +4,46 @@ import pprint
 
 # https://docs.aws.amazon.com/fr_fr/IAM/latest/UserGuide/id_roles_use_switch-role-api.html
 
-# sa
-#AWS_ACCESS_KEY="AKIATDQ7BWJB4GKUIMNS"
-#AWS_SECRET_ACCESS_KEY="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"
-#AWS_SESSION_TOKEN=None
 
-# techaccount1
-AWS_ACCESS_KEY="AKIATDQ7BWJB57OHHHHB"
-AWS_SECRET_ACCESS_KEY="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"
-AWS_SESSION_TOKEN=None
 
 def main():
     print("Allo...")
     pp = pprint.PrettyPrinter(indent=2)
 
-    session = boto3.session.Session(aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, aws_session_token=AWS_SESSION_TOKEN)
+    if len(sys.argv) != 3:
+        print("USAGE: main.py <credential> <bucketName>")
+        sys.exit(1)
+
+    credential = sys.argv[1]
+    bucket_name = sys.argv[2]
+
+    if credential == "sa":
+        session = boto3.session.Session(aws_access_key_id="AKIATDQ7BWJB4GKUIMNS", aws_secret_access_key="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")
+    elif credential == "techaccount1":
+        session = boto3.session.Session(aws_access_key_id="AKIATDQ7BWJB57OHHHHB", aws_secret_access_key="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")
+    elif credential == "none":
+        session = boto3.session.Session()
+    elif credential == "single1_gha1" or credential == "single1_gha2":
+        sts_client = boto3.client('sts')
+        accountId = sts_client.get_caller_identity().get('Account')
+        #print(accountId)
+        assumed_role_object = sts_client.assume_role(
+            RoleArn="arn:aws:iam::{}:role/{}".format(accountId, credential),
+            RoleSessionName = "{}_session".format(credential)
+        )
+        credentials = assumed_role_object['Credentials']
+        session = boto3.session.Session(aws_access_key_id=credentials['AccessKeyId'], aws_secret_access_key=credentials['SecretAccessKey'], aws_session_token=credentials['SessionToken'])
+    else:
+        print("ERROR: credential must be one of 'sa' or 'techaccount1'")
+        sys.exit(2)
 
     s3 = session.client('s3')
 
-    if False:
-        response = s3.list_buckets()
-        #pp.pprint(response)
-        print(response["Buckets"][0]["Name"])
+    # response = s3.list_buckets()
+    # #pp.pprint(response)
+    # print(response["Buckets"][0]["Name"])
 
-    response2 = s3.list_objects_v2(Bucket='gha2-primary-1', FetchOwner=False)
+    response2 = s3.list_objects_v2(Bucket=bucket_name, FetchOwner=False)
     for entry in response2["Contents"]:
         print(entry["Key"])
     #pp.pprint(response2["Contents"])
